af4e0a759ac50253b8a003b2c1021121c766ff00
nitin panwar <nitinpanwawr98@gmail.com>
2017-11-17 21:46:30 -0800

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fridayApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fridayApp
+ */
+angular.module('fridayApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fridayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fridayApp
+ */
+angular.module('fridayApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
